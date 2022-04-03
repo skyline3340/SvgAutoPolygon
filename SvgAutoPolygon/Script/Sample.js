dd78a2d86e5fe6e19217a10be29e1309acc98ab2
skyline3340 <gta3340@gmail.com>
2022-04-03 22:55:09 +0800

    add addLine() function

---
+++ SvgAutoPolygon/Script/Sample.js
@@ -0,0 +1,168 @@
+//polygon's points.
+let polyPoint1 = [
+    { x: 0, y: 1 },
+    { x: 3, y: 2 },
+    { x: 6, y: 1 },
+    { x: 3, y: 0 }
+]
+
+//create polygon by using default style option.
+var polygon1 = new svgModule('div1', null, 300, 300)
+    .addPolygon({
+        id: 'polygon1',
+        points: [
+            { y: -25.774, x: 80.19 },
+            { y: -18.466, x: 66.118 },
+            { y: -40.321, x: 50.757 },
+        ],
+        zoom: 8.5,
+        top: 50,
+        left: 25,
+        fill: 'aqua',
+        stroke: 'white',
+        strokeWidth: 5,
+    })
+    .init();
+
+//create polygon by using style value.
+var polygon2 = new svgModule('div2', null, 300, 300)
+    .addPolygon({
+        id: 'polygon2',
+        points: [
+            { y: -25.774, x: 80.19 },
+            { y: 18.466, x: 66.118 },
+            { y: -40.321, x: 50.757 },
+        ],
+        zoom: 4.5,
+        top: 20,
+        left: 85,
+        style: "fill: red; stroke: white; stroke-width: 5;"
+    })
+    .init();
+
+//create multi polygon by using shift option.
+var polygon3 = new svgModule('div3', null, 300, 300)
+    .addPolygon({
+        points: [
+            { x: 0, y: 5 },
+            { x: 2.5, y: 0 },
+            { x: 5, y: 5 }
+        ],
+        zoom: 40,
+        left: 60,
+        top: 75,
+        style: "fill: none; stroke: white; stroke-width: 5;"
+    })
+    .addPolygon({
+        points: [
+            { x: 0, y: 0 },
+            { x: 2.5, y: 5 },
+            { x: 5, y: 0 }
+        ],
+        zoom: 40,
+        top: 25,
+        left: 60,
+        style: "fill: none; stroke: white; stroke-width: 5;"
+    })
+    .init();
+
+//create circle and drawing something
+var circle1 = new svgModule('div4', null, 300, 300)
+    .addCircle({
+        r: 50,
+        fill: 'red',
+        stroke: 'white',
+        strokeWidth: 5,
+        top: 145,
+        left: 155,
+    })
+    .init();
+
+//create a polyline
+var polyline1 = new svgModule('div5', null, 300, 300)
+    .addLine({
+        points: [
+            { x: 0, y: 0 },
+            { x: 10, y: 10 },
+        ],
+        zoom: 20,
+        top: 50,
+        left: 50,
+        style: "fill: none; stroke: white; stroke-width: 5;"
+    })
+    .init();
+
+//build a icon for layer.
+var example1 = new svgModule('ex1', null, 310, 200)
+    .addPolygon({
+        points: polyPoint1,
+        zoom: 50,
+        style: "fill: #cccccc; stroke: black; stroke-width: 8;"
+    })
+    .addPolygon({
+        points: polyPoint1,
+        zoom: 50,
+        top: 50,
+        style: "fill: #a6a6a6; stroke: black; stroke-width: 8;"
+    })
+    .addPolygon({
+        points: polyPoint1,
+        zoom: 50,
+        top: 95,
+        style: "fill: #8c8c8c; stroke: black; stroke-width: 8;"
+    })
+    .init();
+
+//build a icon for power button.
+var example2 = new svgModule('ex2', null, 300, 200)
+    .addPolygon({
+        points: [
+            { x: 0, y: 0 },
+            { x: 0, y: 15 },
+            { x: 5, y: 15 },
+            { x: 5, y: 0 },
+        ],
+        zoom: 5,
+        top: 35,
+        left: 141,
+        style: "fill: white; stroke: black; stroke-width: 8;",
+    })
+    .addCircle({
+        r: 50,
+        top: 113,
+        left: 153,
+        style: "fill: black; stroke: white; stroke-width: 10;"
+    })
+    .init();
+
+//build a smile icon
+var example3 = new svgModule('ex3', null, 300, 110)
+    .addLine({
+        points: [
+            { x: 0, y: 0 },
+            { x: 0, y: 5 }
+        ],
+        top: 30,
+        left: 100,
+        zoom: 10,
+        style: "fill: none; stroke: white; stroke-width: 15;"
+    })
+    .addLine({
+        points: [
+            { x: 0, y: 5 },
+            { x: 0, y: 0 }
+        ],
+        top: 30,
+        left: 210,
+        zoom: 10,
+        style: "fill: none; stroke: white; stroke-width: 15;"
+    }).init();
+
+var example3_1 = new svgModule('ex3', null, 300, 110)
+    .addCircle({
+        r: 100,
+        top: -20,
+        left: 153,
+        style: "fill: none; stroke: white; stroke-width: 15;"
+    })
+    .init();
--- SvgAutoPolygon/Script/SvgModule.js
+++ SvgAutoPolygon/Script/SvgModule.js
@@ -16,7 +16,7 @@ class svgModule {
     //id: polygon object's id (default = null)
     //points: polygon object's points by using [{x: value, y: value},......]
     //zoom: value for zoom (can use both int and string, default = 1)
-    //fill: color for fill (style, default = 'white')
+    //fill: color for fill (style, default = 'none')
     //stroke: color for stroke (style, default = 'black')
     //strokeWidth: value for stroke-width (style, can use both int and string, default = 1)
     //style: edit element's style by string (can just use this to edit style)
@@ -26,7 +26,7 @@ class svgModule {
     addPolygon(opt) {
         opt.strokeWidth = opt.strokeWidth ?? 1;
         opt.stroke = opt.stroke ?? 'black';
-        opt.fill = opt.fill ?? 'white';
+        opt.fill = opt.fill ?? 'none';
         opt.zoom = opt.zoom ?? 1;
 
         this.obj.push(document.createElementNS("http://www.w3.org/2000/svg", 'polygon'));
@@ -55,19 +55,19 @@ class svgModule {
         return this;
     }
 
-    //opt{id, r, top?, left?, fill?, stroke?, strokeWidth?, style?}
+    //opt{id?, r, top?, left?, fill?, stroke?, strokeWidth?, style?}
     //id: circle object's id (default = null)
-    //fill: color for fill (style, default = 'white')
+    //fill: color for fill (style, default = 'none')
     //stroke: color for stroke (style, default = 'black')
     //strokeWidth: value for stroke-width (style, can use both int and string, default = 1)
     //style: edit element's style by string (can just use this to edit style)
     //top: value for shift circle (default = stroke-width + r)
     //left: value for shift circle (default = stroke-width + r)
     //* when style has been setted, it will overwrite every style's setting before, even with default value.
-    addCircle(opt){
+    addCircle(opt) {
         opt.strokeWidth = opt.strokeWidth ?? 1;
         opt.stroke = opt.stroke ?? 'black';
-        opt.fill = opt.fill ?? 'white';
+        opt.fill = opt.fill ?? 'none';
 
         this.obj.push(document.createElementNS("http://www.w3.org/2000/svg", 'circle'));
         var len = this.obj.length - 1;
@@ -82,6 +82,39 @@ class svgModule {
         this.obj[len].setAttribute('r', opt.r);
         this.obj[len].setAttribute('cx', opt.left);
         this.obj[len].setAttribute('cy', opt.top);
+
+        return this;
+    }
+
+    //opt{id?, points, zoom?, fill?, stroke?, strokeWidth?, style?, top?, left?}
+    addLine(opt) {
+        opt.strokeWidth = opt.strokeWidth ?? 1;
+        opt.stroke = opt.stroke ?? 'black';
+        opt.fill = opt.fill ?? 'none';
+        opt.zoom = opt.zoom ?? 1;
+
+        this.obj.push(document.createElementNS("http://www.w3.org/2000/svg", 'polyline'));
+        var len = this.obj.length - 1;
+        (opt.id != null) ? this.obj[len].setAttribute('id', opt.id) : null;
+        this.obj[len].style['stroke-width'] = `${opt.strokeWidth}`;
+        this.obj[len].style['stroke'] = opt.stroke;
+        this.obj[len].style['fill'] = opt.fill;
+        (opt.style != null) ? (this.obj[len].style = opt.style) : null;
+
+        opt.top = opt.top ?? parseInt(this.obj[len].style['stroke-width']);
+        opt.left = opt.left ?? parseInt(this.obj[len].style['stroke-width']);
+
+        var xMin = opt.points.map(p => p.x).sort(function (a, b) { return a - b })[0];
+        var yMax = opt.points.map(p => p.y).sort(function (a, b) { return b - a })[0];
+
+        var svgPoints = "";
+        for (var i = 0; i < opt.points.length; i++) {
+            var x = (opt.points[i].x - xMin) * opt.zoom;
+            var y = (opt.points[i].y - yMax) * opt.zoom;
+            svgPoints += `${(x >= 0) ? x + opt.left : x * -1 + opt.left},${(y >= 0) ? y + opt.top : y * -1 + opt.top} `;
+        }
+
+        this.obj[len].setAttribute('points', svgPoints);
 
         return this;
     }
